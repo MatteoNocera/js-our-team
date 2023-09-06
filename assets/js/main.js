7e988bf2cd21e9e63c45d1ba6a25708ec5078f80
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-06 15:33:41 +0200

    prepare html and markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,10 +45,24 @@ Tools :
 - querySelector
 - insertAdiacentHTML
 
+markup            <div class="row row-cols-1 row-cols-md-3 g-4">
+                            <div class="col">
+                                <div class="card h-100">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Name</h5>
+                                        <p class="card-text">Role</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                    </div>
 
 */
 
 // creare un array di oggetti del team con seguenti informazioni: nome, ruolo e foto
+
+
 
 // Creare l’array di oggetti con le informazioni fornite
 
